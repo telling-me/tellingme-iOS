405b67c854d6be05c9b40a30b4d794a0722fe844
akrudal <lottie125@naver.com>
2023-03-06 16:33:48 +0900

    [chore] : 프로젝트 생성
    1
    chore

---
+++ TellingMe/tellingMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TellingMe
+//
+//  Created by 마경미 on 06.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
