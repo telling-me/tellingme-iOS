fd60ee1141427c006fb6f3b27609c2ce2cc7a662
akrudal <lottie125@naver.com>
2023-03-15 19:49:44 +0900

    [style] : add font - 2 나눔스퀘어라운드 폰트 적용 2 style

--- TellingMe/tellingMe/presentation/ViewController.swift
+++ TellingMe/tellingMe/presentation/ViewController.swift
@@ -6,12 +6,42 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        labelAnimate()
+    }
+    
+    func labelAnimate() {
+        let now = DispatchTime.now()
+        DispatchQueue.main.asyncAfter(deadline: now + 1) {
+            self.pushAnimate(label: self.label1)
+        }
+        DispatchQueue.main.asyncAfter(deadline: now + 2) {
+            self.pushAnimate(label: self.label2)
+        }
+        DispatchQueue.main.asyncAfter(deadline: now + 3) {
+            self.label3.font = UIFont(name: "NanumSquareRoundOTFB", size: 26)
+            self.pushAnimate(label: self.label3)
+        }
     }
 
+    private func pushAnimate(label: UILabel) {
+        label.isHidden = false
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = .init(name: .easeInEaseOut)
+        transition.type = .push
+        transition.subtype = .fromTop
+        label.layer.add(transition, forKey: CATransitionType.push.rawValue)
+    }
 }
