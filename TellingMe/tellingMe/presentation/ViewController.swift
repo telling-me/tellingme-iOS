b9631afd71696adaf3b6acf578d4dff926ff0355
akrudal <lottie125@naver.com>
2023-03-18 01:26:29 +0900

    [feat] : apple login - 8 - 애플 로그인 서버 연동 전까지 작업 완료 - 브랜치 잘 못 작업했습니다.. ㅜㅜ 9 feat

--- TellingMe/tellingMe/presentation/ViewController.swift
+++ TellingMe/tellingMe/presentation/ViewController.swift
@@ -16,17 +16,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var loginStackView: UIStackView!
 
-    @IBAction func clickKakaoLogin(_ sender: UIButton) {
-        if (UserApi.isKakaoTalkLoginAvailable()) {
+    @objc
+    func clickKakaoLogin() {
+        if UserApi.isKakaoTalkLoginAvailable() {
             UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
                 if let error = error {
                     print(error)
-                }
-                else {
+                } else {
                     print("loginWithKakaoTalk() success.")
-
-                    //do something
+                    // do something
                     _ = oauthToken
                 }
             }
@@ -36,7 +36,6 @@ class ViewController: UIViewController {
                     print(error)
                 } else {
                     print("카카오 계정으로 로그인 성공")
-                    
                     _ = oauthToken
                     // 관련 메소드 추가
                 }
@@ -44,14 +43,54 @@ class ViewController: UIViewController {
         }
     }
 
+    @objc
+    func clickAppleLogin() {
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+
+      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.delegate = self
+      authorizationController.presentationContextProvider = self
+      authorizationController.performRequests()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLoginButtons()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         labelAnimate()
     }
-    
+
+    func setLoginButtons() {
+        let kakaoButton: UIButton = {
+            let button = UIButton()
+
+            button.titleLabel?.text = "카카오 계정으로 시작하기"
+            button.addTarget(self, action: #selector(clickKakaoLogin), for: .touchDown)
+            button.backgroundColor = .black
+            button.translatesAutoresizingMaskIntoConstraints = false
+            return button
+        }()
+
+        let appleButton: ASAuthorizationAppleIDButton = {
+            let button = ASAuthorizationAppleIDButton(type: .continue, style: .black)
+            button.addTarget(self, action: #selector(clickAppleLogin), for: .touchDown)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            return button
+        }()
+
+        kakaoButton.heightAnchor.constraint(equalToConstant: 54).isActive = true
+        appleButton.heightAnchor.constraint(equalToConstant: 54).isActive = true
+        loginStackView.addArrangedSubview(kakaoButton)
+        loginStackView.addArrangedSubview(appleButton)
+
+        kakaoButton.layer.cornerRadius = kakaoButton.frame.height / 2
+        appleButton.layer.cornerRadius = appleButton.frame.height / 2
+    }
+
     func labelAnimate() {
         let now = DispatchTime.now()
         DispatchQueue.main.asyncAfter(deadline: now + 1) {
@@ -74,5 +113,20 @@ class ViewController: UIViewController {
         transition.type = .push
         transition.subtype = .fromTop
         label.layer.add(transition, forKey: CATransitionType.push.rawValue)
+    }
+}
+
+extension ViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window ?? UIWindow()
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        let appleIDCredential = authorization.credential
+        print("AppleID Credential Authorization: \(appleIDCredential))")
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+      // Handle error.
     }
 }
