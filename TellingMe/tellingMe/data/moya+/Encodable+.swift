d85661d73d609afe10d4788659e1cd5b69e129c5
akrudal <lottie125@naver.com>
2023-03-20 16:33:06 +0900

    [test] : api test - 9 - swagger 보고 api test - moya 라이브러리를 통한 통신 - header, json 통신 형식 나오면 수정해야 함 9 test

---
+++ TellingMe/tellingMe/data/moya+/Encodable+.swift
@@ -0,0 +1,8 @@
+//
+//  Encodable+.swift
+//  tellingMe
+//
+//  Created by 마경미 on 20.03.23.
+//
+
+import Foundation
---
+++ TellingMe/tellingMe/data/test/DataSource/TestDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  TestDataSource.swift
+//  tellingMe
+//
+//  Created by 마경미 on 20.03.23.
+//
+
+import Foundation
