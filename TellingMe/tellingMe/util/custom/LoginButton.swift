8e046cff1fb30c5f38637e2f85e65594fdf371f8
akrudal <lottie125@naver.com>
2023-03-23 00:22:00 +0900

    [feat] : kakao_login_token - 13 - hastoken - userInfo 13 feat

---
+++ TellingMe/tellingMe/util/custom/LoginButton.swift
@@ -0,0 +1,32 @@
+//
+//  LoginButton.swift
+//  tellingMe
+//
+//  Created by 마경미 on 22.03.23.
+//
+
+import UIKit
+
+class LoginButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    func initializeLabel(name: String, color: UIColor) {
+        self.setTitle("\(name)로 계속하기", for: .normal)
+        self.backgroundColor = color
+        self.setTitleColor(UIColor.black, for: .normal)
+        self.titleLabel?.font = UIFont(name: "NanumSquareRoundOTFR", size: 15)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if name == "Apple" {
+            self.setTitleColor(UIColor.white, for: .normal)
+            self.setImage(UIImage(named: "apple"), for: .normal)
+            self.imageEdgeInsets = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 12)
+        }
+    }
+}
