b9b917473b462039f1693c3f5899598b2f4e693b
akrudal <lottie125@naver.com>
2023-04-28 23:08:06 +0900

    [style] : add customTabBar

--- TellingMe/tellingMe/presentation/tab/MainTabBarController.swift
+++ TellingMe/tellingMe/presentation/tab/MainTabBarController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
+        tabBar.frame = CGRect(x: 0, y: view.frame.height - 88, width: view.frame.width, height: 88)
+
         super.viewDidLoad()
         selectedIndex = 1
 
         tabBar.layer.cornerRadius = 32
         tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-
+//        let shadowView = UIView(frame: tabBar.frame)
+//        shadowView.frame = tabBar.bounds
+//        print(tabBar)
+//        print(shadowView)
+//        shadowView.backgroundColor = .white
+//        shadowView.layer.cornerRadius = 32
+//        shadowView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.08).cgColor
+//        shadowView.layer.shadowOpacity = 1
+//        shadowView.layer.shadowOffset = CGSize(width: 0, height: 4)
+//        shadowView.layer.shadowRadius = 20
+//        shadowView.layer.masksToBounds = false
+//        // clipsToBounds 속성을 false로 설정합니다.
+//        tabBar.clipsToBounds = false
+//        print(tabBar.superview)
+//        tabBar.superview?.insertSubview(shadowView, aboveSubview: tabBar)
         let shadowView = UIView(frame: tabBar.frame)
         shadowView.backgroundColor = .white
         shadowView.layer.cornerRadius = 32
@@ -24,9 +40,8 @@ class MainTabBarController: UITabBarController {
         shadowView.layer.shadowRadius = 20
         shadowView.layer.masksToBounds = false
 
-        // clipsToBounds 속성을 false로 설정합니다.
-        tabBar.clipsToBounds = false
-        tabBar.superview?.insertSubview(shadowView, belowSubview: tabBar)
+        self.view.addSubview(shadowView)
+        self.view.bringSubviewToFront(self.tabBar)
     }
 
     override func viewDidLayoutSubviews() {
