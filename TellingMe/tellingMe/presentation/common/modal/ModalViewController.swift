b9e36074d0ff0b2168d44ac6c3bf6fe8f78e557a
akrudal <lottie125@naver.com>
2023-05-22 16:18:30 +0900

    [style] : modal - 49 - 답변 삭제 모달 - 답변 등록 모달 49 style

---
+++ TellingMe/tellingMe/presentation/common/modal/ModalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModalViewController.swift
+//  tellingMe
+//
+//  Created by 마경미 on 21.05.23.
+//
+
+import UIKit
+
+protocol ModalActionDelegate: AnyObject {
+    func clickCancel()
+    func clickOk()
+}
+
+class ModalViewController: UIViewController {
+    weak var delegeate: ModalActionDelegate?
+    @IBOutlet weak var cancelButton: TeritaryTextButton!
+    @IBOutlet weak var okButton: SecondaryTextButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func clickButton(_ sender: UIButton) {
+        // 취소 버튼 tag = 0, else = 1
+        if sender.tag == 0 {
+            self.dismiss(animated: true)
+            self.delegeate?.clickCancel()
+        } else {
+            self.dismiss(animated: true)
+            self.delegeate?.clickOk()
+        }
+    }
+}
