9b5d4898a2360f0aa63597e7f3954ea7934d4cb0
Kyoungmi <1mmgm.dev@gmail.com>
2023-06-21 01:19:05 +0900

    [feedback] add_agreements-79:

---
+++ TellingMe/tellingMe/presentation/signUp/page/AgreementDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AgreementDetailViewController.swift
+//  tellingMe
+//
+//  Created by 마경미 on 20.06.23.
+//
+
+import Foundation
+import UIKit
+
+class AgreementDetailViewController1: UIViewController {
+    override func viewDidLoad() {
+    }
+
+    @IBAction func close(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
+
+class AgreementDetailViewController2: UIViewController {
+    override func viewDidLoad() {
+    }
+
+    @IBAction func close(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
