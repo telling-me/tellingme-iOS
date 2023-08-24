b122c22e7801b16c8c29fce893ee3a75abe0825d
PecanPie <hidra0321@naver.com>
2023-08-24 23:15:21 +0900

    Added files

---
+++ TellingMe/tellingMe/presentation/alarm/views/AlarmNavigationBarView.swift
@@ -0,0 +1,20 @@
+//
+//  AlarmNavigationBarView.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/08/24.
+//
+
+import UIKit
+
+class AlarmNavigationBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TellingMe/tellingMe/presentation/alarm/views/AlarmReadAllSectionView.swift
@@ -0,0 +1,20 @@
+//
+//  AlarmReadAllSectionView.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/08/24.
+//
+
+import UIKit
+
+class AlarmReadAllSectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
