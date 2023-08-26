79c4c66a958f43dd577e74f473aa5af7c8b679cf
PecanPie <hidra0321@naver.com>
2023-08-26 20:33:49 +0900

    Added files

---
+++ TellingMe/tellingMe/presentation/myPage/viewModel/MyPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageViewModel.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/08/26.
+//
+
+import Foundation
---
+++ TellingMe/tellingMe/presentation/myPage/views/MyPageProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageProfileView.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/08/26.
+//
+
+import UIKit
+
+class MyPageProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
