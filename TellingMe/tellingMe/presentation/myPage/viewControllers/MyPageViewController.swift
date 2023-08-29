32430dd4ea79053969151007d21ab18d3820d8de
PecanPie <hidra0321@naver.com>
2023-08-29 23:55:18 +0900

    tablecell for setting view is added

--- TellingMe/tellingMe/presentation/myPage/viewControllers/MyPageViewController.swift
+++ TellingMe/tellingMe/presentation/myPage/viewControllers/MyPageViewController.swift
@@ -7,9 +7,34 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
 final class MyPageViewController: UIViewController {
 
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
+        setLayout()
+        setStyles()
+    }
+}
+
+extension MyPageViewController {
+    
+    private func bindViewModel() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    private func setStyles() {
+        
     }
 }
--- TellingMe/tellingMe/presentation/myPage/views/components/MyPageTableViewCell.swift
+++ TellingMe/tellingMe/presentation/myPage/views/components/MyPageTableViewCell.swift
@@ -7,6 +7,91 @@
 
 import UIKit
 
-final class MyPageTableViewCell: UITableViewCell {
+import SnapKit
+import Then
 
+final class MyPageTableViewCell: UITableViewCell {
+    
+    private var isToggleCell: Bool = false
+    private var isLogoutCell: Bool = false
+    
+    private let cellTitleLabel = UILabel()
+    private let chevronImageView = UIImageView()
+    let notificationToggle = UISwitch()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayout()
+        setStyles()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if isToggleCell != false {
+            
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MyPageTableViewCell {
+    
+    private func setStyles() {
+        self.backgroundColor = .clear
+        
+        cellTitleLabel.do {
+            $0.font = .fontNanum(.B1_Regular)
+            $0.textColor = .Gray8
+        }
+        
+        notificationToggle.do {
+            $0.isOn = false
+            $0.preferredStyle = .sliding
+            $0.onTintColor = .Logo
+        }
+        
+        chevronImageView.do {
+            $0.image = UIImage(named: "chevronNext")
+            $0.contentMode = .scaleAspectFit
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubviews(cellTitleLabel, chevronImageView)
+        
+        cellTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(25)
+            $0.centerY.equalToSuperview()
+        }
+        
+        chevronImageView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(21)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
+
+extension MyPageTableViewCell {
+    private func setSwitchView() {
+        chevronImageView.removeFromSuperview()
+        self.addSubview(notificationToggle)
+        
+        notificationToggle.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(21)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(50)
+            $0.height.equalTo(30)
+        }
+    }
+}
+
+extension MyPageTableViewCell {
+    func configure(title: String, isToggleCell: Bool = false, isLogoutCell: Bool = false) {
+        cellTitleLabel.text = title
+        self.isToggleCell = isToggleCell
+        self.isLogoutCell = isLogoutCell
+    }
 }
