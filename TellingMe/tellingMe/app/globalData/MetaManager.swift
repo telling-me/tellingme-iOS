570c288ea9e5104d0f7b97f9fe8e6ac8385a4c65
PecanPie <hidra0321@naver.com>
2023-09-03 21:09:07 +0900

    Added appId Key for Meta

---
+++ TellingMe/tellingMe/app/globalData/MetaManager.swift
@@ -0,0 +1,21 @@
+//
+//  MetaManager.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/09/03.
+//
+
+import UIKit
+
+final class MetaManager {
+
+    
+    
+}
+
+
+
+//var baseURL: URL {
+//    guard let apiURL = URL(string: Bundle.main.APIURL) else { fatalError("URL is invalid") }
+//    return apiURL
+//}
