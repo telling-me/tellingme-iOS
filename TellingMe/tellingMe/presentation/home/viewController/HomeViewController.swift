b48db37809821e03e74e8e04d69e9c78e29cb60e
PecanPie <hidra0321@naver.com>
2023-09-04 22:31:11 +0900

    Added NotificationCenter for refresh when become active

--- TellingMe/tellingMe/presentation/home/viewController/HomeViewController.swift
+++ TellingMe/tellingMe/presentation/home/viewController/HomeViewController.swift
@@ -32,6 +32,7 @@ class HomeViewController: UIViewController {
         setView()
         checkNofitication()
         bindViewModel()
+        setNotificationCenterForBecomeActive()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -62,6 +63,10 @@ class HomeViewController: UIViewController {
         for animationView in animationViews {
             animationView.layer.removeAllAnimations()
         }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
     func setView() {
@@ -136,10 +141,7 @@ class HomeViewController: UIViewController {
     }
 }
 
-extension HomeViewController: HeaderViewDelegate, AlarmReadAllTappedProtocol {
-    func readAllTapped() {
-        self.headerView.alarmNoticeButton.setImage(UIImage(named: "NoticeAlarm"), for: .normal)
-    }
+extension HomeViewController: HeaderViewDelegate {
     
     func pushAlarmNotice(_ headerView: MainHeaderView) {
         let vc = AlarmViewController()
@@ -177,5 +179,32 @@ extension HomeViewController: HeaderViewDelegate, AlarmReadAllTappedProtocol {
                 self?.showPushNotification()
             })
             .disposed(by: disposeBag)
+    }
+}
+
+extension HomeViewController {
+    private func setNotificationCenterForBecomeActive() {
+        print("NotificationCenter Added for background check.")
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshNetwork), name: Notification.Name("RefreshHomeView"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshAnimation), name: Notification.Name("RefreshAnimation"), object: nil)
+    }
+    
+    private func restartAnimation() {
+        animation()
+    }
+}
+
+extension HomeViewController {
+    @objc
+    private func refreshNetwork() {
+        print("Back From Background, refreshed the question.")
+        getQuestion()
+    }
+    
+    @objc
+    private func refreshAnimation() {
+        print("Back From Background, refreshed the animation.")
+        /// This doesn't work.
+        restartAnimation()
     }
 }
