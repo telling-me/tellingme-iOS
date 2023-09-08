63a2dc3cab2302cb64cfe50ff124a4ae7f9c6685
PecanPie <hidra0321@naver.com>
2023-09-08 20:05:50 +0900

    Added HeaderView for mypage

--- TellingMe/tellingMe/presentation/myPage/views/MyPageHeaderView.swift
+++ TellingMe/tellingMe/presentation/myPage/views/MyPageHeaderView.swift
@@ -12,9 +12,11 @@ import Then
 
 final class MyPageHeaderView: UIView {
 
+    let backButton = UIButton()
+    private let titleLabel = UILabel()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        bindViewModel()
         setLayout()
         setStyles()
     }
@@ -25,16 +27,34 @@ final class MyPageHeaderView: UIView {
 }
 
 extension MyPageHeaderView {
+    
+    private func setStyles() {
+        self.backgroundColor = .Side100
 
-    private func bindViewModel() {
+        backButton.do {
+            $0.setImage(UIImage(named: "ArrowLeft"), for: .normal)
+        }
         
+        titleLabel.do {
+            $0.text = "마이페이지"
+            $0.textColor = .Gray6
+            $0.font = .fontNanum(.H6_Bold)
+            $0.textAlignment = .center
+        }
     }
     
     private func setLayout() {
+        self.addSubviews(backButton, titleLabel)
         
-    }
-    
-    private func setStyles() {
+        backButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(21)
+            $0.top.equalToSuperview().inset(20)
+            $0.size.equalTo(24)
+        }
         
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(backButton.snp.centerY)
+        }
     }
 }
