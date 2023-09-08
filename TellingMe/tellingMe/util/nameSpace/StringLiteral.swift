bb5a2a370e98f5e07a5939a19a464c82d0d05747
PecanPie <hidra0321@naver.com>
2023-09-08 19:53:02 +0900

    Added Device Literals for exceptional devices regarding of UI Crash

--- TellingMe/tellingMe/util/nameSpace/StringLiteral.swift
+++ TellingMe/tellingMe/util/nameSpace/StringLiteral.swift
@@ -16,53 +16,53 @@ enum StringLiterals {
 
 enum ArrayLiterals {}
 
-//enum DeviceLiterals: CaseIterable {
-//    case six
-//    case sixPlus
-//    case sixS
-//    case sixSPlus
-//    case seven
-//    case sevenPlus
-//    case eight
-//    case eightPlus
-//    case seOne
-//    case seTwo
-//    case seThree
-//    case iPadMiniFour
-//    case iPadMiniFive
-//    case iPadMiniSix
-////    case iPadMiniSeven
-//    
-//    var deviceName: String {
-//        switch self {
-//        case .six:
-//            return "iPhone 6"
-//        case .sixPlus:
-//            return "iPhone 6 Plus"
-//        case .sixS:
-//            return "iPhone 6s"
-//        case .sixSPlus:
-//            return "iPhone 6s Plus"
-//        case .seven:
-//            return "iPhone 7"
-//        case .sevenPlus:
-//            return "iPhone 7 Plus"
-//        case .eight:
-//            return "iPhone 8"
-//        case .eightPlus:
-//            return "iPhone 8 Plus"
-//        case .seOne:
-//            return "iPhone SE (1st generation)"
-//        case .seTwo:
-//            return "iPhone SE (2nd generation)"
-//        case .seThree:
-//            return "iPhone SE (3rd generation)"
-//        case .iPadMiniFour:
-//            return "iPad mini 4"
-//        case .iPadMiniFive:
-//            return "iPad mini (5th generation)"
-//        case .iPadMiniSix:
-//            return "iPad mini (6th generation)"
-//        }
-//    }
-//}
+enum DeviceLiterals: CaseIterable {
+    case six
+    case sixPlus
+    case sixS
+    case sixSPlus
+    case seven
+    case sevenPlus
+    case eight
+    case eightPlus
+    case seOne
+    case seTwo
+    case seThree
+    case iPadMiniFour
+    case iPadMiniFive
+    case iPadMiniSix
+//    case iPadMiniSeven
+    
+    var deviceName: String {
+        switch self {
+        case .six:
+            return "iPhone 6"
+        case .sixPlus:
+            return "iPhone 6 Plus"
+        case .sixS:
+            return "iPhone 6s"
+        case .sixSPlus:
+            return "iPhone 6s Plus"
+        case .seven:
+            return "iPhone 7"
+        case .sevenPlus:
+            return "iPhone 7 Plus"
+        case .eight:
+            return "iPhone 8"
+        case .eightPlus:
+            return "iPhone 8 Plus"
+        case .seOne:
+            return "iPhone SE (1st generation)"
+        case .seTwo:
+            return "iPhone SE (2nd generation)"
+        case .seThree:
+            return "iPhone SE (3rd generation)"
+        case .iPadMiniFour:
+            return "iPad mini 4"
+        case .iPadMiniFive:
+            return "iPad mini (5th generation)"
+        case .iPadMiniSix:
+            return "iPad mini (6th generation)"
+        }
+    }
+}
