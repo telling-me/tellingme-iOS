c1c11dc86d46794956d143a0693331dcd98a7781
PecanPie <hidra0321@naver.com>
2023-09-08 19:50:24 +0900

    Added New MyPageCell with Toggle

---
+++ TellingMe/tellingMe/presentation/myPage/views/components/MyPageToggleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageToggleTableViewCell.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/09/06.
+//
+
+import UIKit
+
+class MyPageToggleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
