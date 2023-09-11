e5690553717c3f565f5e91688facb953b52dba80
PecanPie <hidra0321@naver.com>
2023-09-12 00:57:42 +0900

    Added EmailFeedBack VC to present Emailing VC

--- TellingMe/tellingMe/presentation/common/feedbackMail/EmailFeedbackViewController.swift
+++ TellingMe/tellingMe/presentation/common/feedbackMail/EmailFeedbackViewController.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import MessageUI
 
 class EmailFeedbackViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension EmailFeedbackViewController: MFMailComposeViewControllerDelegate {
+    func sendFeedbackMail(userOf user: String?) {
+        if MFMailComposeViewController.canSendMail() {
+            let versionManager = VersionManager()
+            guard let appVersion: String = versionManager.getCurrentVersion(), let userName = user else { return }
+
+            let mailViewController = MFMailComposeViewController()
+            let toMail = "tellingmetime@gmail.com"
+            let message = """
+                          
+                          안녕하세요, 텔링미입니다.
+                          어떤 내용을 텔링미에게 전달하고 싶으신가요? 자유롭게 작성해주시면 확인 후 답변 드리겠습니다. 감사합니다.😃
+                          📱 쓰고 있는 핸드폰 기종 (예:아이폰 12):
+                          
+                          🧭 앱 버전: \(appVersion)
+                          🧗🏻‍♀️ 닉네임: \(userName)
+                          
+                          ⚠️ 오류를 발견하셨을 경우 ⚠️
+                          📍발견한 오류 :
+                          
+                          📷 오류 화면 (캡쳐 혹은 화면녹화):
+
+                          
+                          """
+            mailViewController.mailComposeDelegate = self
+            mailViewController.setToRecipients([toMail])
+            mailViewController.setSubject("")
+            mailViewController.setMessageBody(message, isHTML: false)
+            self.present(mailViewController, animated: true)
+        } else {
+            print("Popping the EmailVC up failed.")
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
-    */
-
 }
