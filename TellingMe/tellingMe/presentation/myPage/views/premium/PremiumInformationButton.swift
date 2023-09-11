c0053d6e38ff91bec5b4ec3de67d607339b695ce
PecanPie <hidra0321@naver.com>
2023-09-12 01:01:38 +0900

    Added Custom Premium UIBUtton for later

--- TellingMe/tellingMe/presentation/myPage/views/premium/PremiumInformationButton.swift
+++ TellingMe/tellingMe/presentation/myPage/views/premium/PremiumInformationButton.swift
@@ -7,14 +7,27 @@
 
 import UIKit
 
-class PremiumInformationButton: UIView {
+final class PremiumInformationButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyles()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension PremiumInformationButton {
+    
+    private func setStyles() {
+        self.isUserInteractionEnabled = false
+        self.isEnabled = false
+        self.cornerRadius = 20
+        self.backgroundColor = .Primary25
+        self.setTitle("프리미엄 모드 출시 준비 중이에요!", for: .normal)
+        self.setTitleColor(.Logo, for: .normal)
+        self.titleLabel?.font = .fontNanum(.H6_Regular)
+    }
 }
