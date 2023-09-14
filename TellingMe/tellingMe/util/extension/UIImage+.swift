e6a2362a8a5b250739569881c34c68932ad2917d
PecanPie <hidra0321@naver.com>
2023-09-15 02:31:09 +0900

    Added UIImage Extension for method that gives inset for UIImage.

--- TellingMe/tellingMe/util/extension/UIImage+.swift
+++ TellingMe/tellingMe/util/extension/UIImage+.swift
@@ -5,4 +5,20 @@
 //  Created by KYUBO A. SHIM on 2023/09/14.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    func withInset(_ insets: UIEdgeInsets) -> UIImage? {
+        let cgSize = CGSize(width: self.size.width + insets.left * self.scale + insets.right * self.scale,
+                            height: self.size.height + insets.top * self.scale + insets.bottom * self.scale)
+        
+        UIGraphicsBeginImageContextWithOptions(cgSize, false, self.scale)
+        defer { UIGraphicsEndImageContext() }
+        
+        let origin = CGPoint(x: insets.left * self.scale, y: insets.top * self.scale)
+        self.draw(at: origin)
+        
+        return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(self.renderingMode)
+    }
+}
+
