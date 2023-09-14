b3c524483097118e1306ed9333f0f165e42375d4
PecanPie <hidra0321@naver.com>
2023-09-15 02:46:20 +0900

    Added SharingBottom Table Cell

--- TellingMe/tellingMe/presentation/answerList/view/sharingToInstagram/SharingToInstagramTableViewCell.swift
+++ TellingMe/tellingMe/presentation/answerList/view/sharingToInstagram/SharingToInstagramTableViewCell.swift
@@ -7,17 +7,48 @@
 
 import UIKit
 
-class SharingToInstagramTableViewCell: UITableViewCell {
+import SnapKit
+import Then
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+final class SharingToInstagramTableViewCell: UITableViewCell {
+
+    private let titleLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setStyles()
+        setLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+}
 
+extension SharingToInstagramTableViewCell {
+    
+    private func setStyles() {
+        self.backgroundColor = .Side100
+        self.selectionStyle = .none
+        
+        titleLabel.do {
+            $0.font = .fontNanum(.B2_Regular)
+            $0.textColor = .Gray8
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(28)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
+
+extension SharingToInstagramTableViewCell {
+    func configure(title: String) {
+        self.titleLabel.text = title
+    }
 }
