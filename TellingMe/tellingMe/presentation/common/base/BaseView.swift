2af6e571cbee5fbd876499c4fc6a203549d07132
PecanPie <hidra0321@naver.com>
2023-09-23 18:38:17 +0900

    Base Files Added

---
+++ TellingMe/tellingMe/presentation/common/base/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/09/23.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
