8520baa8087283ddb0f3051e4cc751f8605d866b
PecanPie <hidra0321@naver.com>
2023-10-01 02:31:16 +0900

    Added a uibutton components to exit the app

--- TellingMe/tellingMe/presentation/common/button/TerminatingButton.swift
+++ TellingMe/tellingMe/presentation/common/button/TerminatingButton.swift
@@ -10,12 +10,19 @@ import UIKit
 import SnapKit
 import Then
 
-class TerminatingButton: UIButton {
+class TerminatingButton: BBaseButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setStyles()
         setAction()
+    }
+    
+    override func setStyles() {
+        self.backgroundColor = .Primary25
+        self.cornerRadius = 20
+        self.setTitle("확인", for: .normal)
+        self.setTitleColor(.Logo, for: .normal)
+        self.titleLabel?.font = .fontNanum(.H6_Regular)
     }
     
     required init?(coder: NSCoder) {
@@ -24,14 +31,6 @@ class TerminatingButton: UIButton {
 }
 
 extension TerminatingButton {
-    
-    private func setStyles() {
-        self.backgroundColor = .Primary25
-        self.cornerRadius = 20
-        self.setTitle("확인", for: .normal)
-        self.setTitleColor(.Logo, for: .normal)
-        self.titleLabel?.font = .fontNanum(.H6_Regular)
-    }
     
     private func setAction() {
         self.addTarget(self, action: #selector(terminateApp), for: .touchUpInside)
