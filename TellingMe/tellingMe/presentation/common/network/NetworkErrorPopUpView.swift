05d971e07605f8c89100f43ef9218f6c8a916a4f
PecanPie <hidra0321@naver.com>
2023-10-01 02:34:17 +0900

    Added a UIView components that represent network Error View

--- TellingMe/tellingMe/presentation/common/network/NetworkErrorPopUpView.swift
+++ TellingMe/tellingMe/presentation/common/network/NetworkErrorPopUpView.swift
@@ -7,14 +7,62 @@
 
 import UIKit
 
-class NetworkErrorPopUpView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class NetworkErrorPopUpView: BBaseView {
+    
+    private let titleLabel = UILabel()
+    private let subTitleLabel = UILabel()
+    private let mainImageView = UIImageView()
+    private let terminateAppButton = TerminatingButton(frame: .zero)
+    
+    override func setStyles() {
+        self.backgroundColor = .Side100
+        self.cornerRadius = 20
+        
+        titleLabel.do {
+            $0.text = "네트워크 연결에 실패했어요."
+            $0.textColor = .Black
+            $0.textAlignment = .center
+            $0.font = .fontNanum(.B1_Bold)
+        }
+        
+        subTitleLabel.do {
+            $0.text = "네트워크 환경을 확인해주세요."
+            $0.textColor = .Gray6
+            $0.textAlignment = .center
+            $0.font = .fontNanum(.B2_Regular)
+        }
+        
+        mainImageView.do {
+            $0.image = ImageLiterals.WorkInProgress
+            $0.contentMode = .scaleAspectFit
+        }
     }
-    */
-
+    
+    override func setLayout() {
+        self.addSubviews(titleLabel, subTitleLabel, terminateAppButton, mainImageView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(30)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+        }
+        
+        terminateAppButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(55)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+        
+        mainImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(16)
+            $0.bottom.equalTo(terminateAppButton.snp.top).offset(-20)
+            $0.width.equalTo(mainImageView.snp.height)
+        }
+    }
 }
