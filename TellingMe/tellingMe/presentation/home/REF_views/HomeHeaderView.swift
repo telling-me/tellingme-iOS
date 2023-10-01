819a904f75b77f1ea830566d6931ce737f3d9532
PecanPie <hidra0321@naver.com>
2023-10-02 02:46:49 +0900

    [Refactor] Refactored Header UI Components for HomeVC

--- TellingMe/tellingMe/presentation/home/REF_views/HomeHeaderView.swift
+++ TellingMe/tellingMe/presentation/home/REF_views/HomeHeaderView.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
-class HomeHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class HomeHeaderView: BBaseView {
+    
+    let alarmButton = BBaseButton(frame: .zero)
+    let myPageButton = BBaseButton(frame: .zero)
+    private let logoImageView = UIImageView()
+    
+    override func setLayout() {
+        self.backgroundColor = .Side100
+        
+        logoImageView.do {
+            $0.image = ImageLiterals.HomeLogo?.withRenderingMode(.alwaysOriginal)
+            $0.contentMode = .scaleAspectFit
+            $0.backgroundColor = .clear
+        }
+        
+        alarmButton.do {
+            $0.setImage(ImageLiterals.HomeNoticeAlarm, for: .normal)
+            $0.backgroundColor = .clear
+        }
+        
+        myPageButton.do {
+            $0.setImage(ImageLiterals.HomeSetting, for: .normal)
+            $0.backgroundColor = .clear
+        }
     }
-    */
+    
+    override func setStyles() {
+        self.addSubviews(logoImageView, myPageButton, alarmButton)
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(25)
+            $0.width.equalTo(81)
+            $0.height.equalTo(34)
+        }
+        
+        myPageButton.snp.makeConstraints {
+            $0.centerY.equalTo(logoImageView.snp.centerY)
+            $0.size.equalTo(26)
+            $0.trailing.equalToSuperview().inset(21)
+        }
+        
+        alarmButton.snp.makeConstraints {
+            $0.centerY.equalTo(logoImageView.snp.centerY)
+            $0.size.equalTo(26)
+            $0.trailing.equalTo(myPageButton.snp.leading).offset(-12)
+        }
+    }
+}
 
+extension HomeHeaderView {
+    func checkIfNewAlarmsExist(_ check: Bool) {
+        switch check {
+        case true:
+            alarmButton.setImage(ImageLiterals.HomeNoticeAlarmWithDot, for: .normal)
+        case false:
+            alarmButton.setImage(ImageLiterals.HomeNoticeAlarm, for: .normal)
+        }
+    }
 }
