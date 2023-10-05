612566bbeb6a88cb57967e89120edbbd9038da4d
PecanPie / Kyubo Shim <89404664+PecanPiePOS@users.noreply.github.com>
2023-10-05 12:06:38 +0900

    Update BBaseView.swift

--- TellingMe/tellingMe/presentation/common/base/BBaseView.swift
+++ TellingMe/tellingMe/presentation/common/base/BBaseView.swift
@@ -18,11 +18,7 @@ class BBaseView: UIView {
         setStyles()
         setLayout()
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     override func removeFromSuperview() {
         super.removeFromSuperview()
         print("View has been successfully Removed")
@@ -34,4 +30,8 @@ class BBaseView: UIView {
     func setStyles() {}
     /// View 의 Layout 을 set 합니다.
     func setLayout() {}
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
