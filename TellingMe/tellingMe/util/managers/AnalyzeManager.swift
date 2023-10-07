a1160f1662eb1ba39092a18ba70a71c55f8eae43
PecanPie <hidra0321@naver.com>
2023-10-07 23:28:49 +0900

    Same

--- TellingMe/tellingMe/util/managers/AnalyzeManager.swift
+++ TellingMe/tellingMe/util/managers/AnalyzeManager.swift
@@ -5,4 +5,35 @@
 //  Created by KYUBO A. SHIM on 2023/10/06.
 //
 
-import Foundation
+import UIKit
+
+import FirebaseAnalytics
+
+final class GAManager {
+    static let shared = GAManager()
+    
+    private init() {}
+    
+    enum EventType {
+        case screen(screenName: String)
+        
+        var eventName: String {
+            switch self {
+            case .screen:
+                return "screen"
+            }
+        }
+        
+        var parameters: [String: Any]? {
+            switch self {
+            case .screen(let screenName):
+                ["screen": screenName]
+            }
+        }
+    }
+    
+    func logEvent(eventType: EventType) {
+        Analytics.logEvent(eventType.eventName, parameters: eventType.parameters)
+    }
+    
+}
