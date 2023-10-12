da6fa559f6c569c2be024ce5931d9681de1ab3eb
PecanPie <hidra0321@naver.com>
2023-10-12 20:58:02 +0900

    Added plus Emotion List to the viewModel

--- TellingMe/tellingMe/presentation/answer/viewModel/EmotionViewModel.swift
+++ TellingMe/tellingMe/presentation/answer/viewModel/EmotionViewModel.swift
@@ -13,11 +13,22 @@ class EmotionViewModel {
         let text: String
     }
 
-    let emotions = [Emotion(image: "Happy", text: "행복해요"), Emotion(image: "Proud", text: "뿌듯해요"), Emotion(image: "Meh", text: "그저 그래요"), Emotion(image: "Tired", text: "피곤해요"), Emotion(image: "Sad", text: "슬퍼요"), Emotion(image: "Angry", text: "화나요")]
+    var emotions = [Emotion(image: "Happy", text: "행복해요"), Emotion(image: "Proud", text: "뿌듯해요"), Emotion(image: "Meh", text: "그저 그래요"), Emotion(image: "Tired", text: "피곤해요"), Emotion(image: "Sad", text: "슬퍼요"), Emotion(image: "Angry", text: "화나요")]
+    let plusEmotions = [
+        Emotion(image: "Happy", text: "행복해요"), Emotion(image: "Proud", text: "뿌듯해요"), Emotion(image: "Meh", text: "그저 그래요"), Emotion(image: "Tired", text: "피곤해요"), Emotion(image: "Sad", text: "슬퍼요"), Emotion(image: "Angry", text: "화나요"),
+        Emotion(image: "Excited", text: "설레요"), Emotion(image: "Thrilled", text: "신나요"), Emotion(image: "Relaxed", text: "편안해요"), Emotion(image: "Lethargic", text: "무기력해요"), Emotion(image: "Lonely", text: "외로워요"), Emotion(image: "Complicated", text: "복잡해요")
+    ]
     var selectedEmotion: Int? = nil
     let emotionCount: Int
 
     init() {
+        switch IAPManager.getHasUserPurchased() {
+        case true:
+            emotions = plusEmotions
+        case false:
+            break
+        }
         emotionCount = emotions.count
+        print(emotionCount, "🚩 emotion count")
     }
 }
