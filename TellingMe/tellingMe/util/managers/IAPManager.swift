79c987267ff85cd97bff79a896f21f78cf9945b3
PecanPie <hidra0321@naver.com>
2023-10-12 20:56:42 +0900

    Added IAP Manager to get whether the user is subscribing

--- TellingMe/tellingMe/util/managers/IAPManager.swift
+++ TellingMe/tellingMe/util/managers/IAPManager.swift
@@ -5,4 +5,16 @@
 //  Created by KYUBO A. SHIM on 2023/10/12.
 //
 
-import Foundation
+import UIKit
+
+final class IAPManager {
+    
+    static func getHasUserPurchased() -> Bool {
+        let userDefaults = UserDefaults.standard
+        if userDefaults.string(forKey: StringLiterals.paidProductId) != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+}
