184126dbba5a853877909dce76cf48f9ba1b5c6a
PecanPie <hidra0321@naver.com>
2023-10-13 14:30:38 +0900

    Added files

---
+++ TellingMe/tellingMe/presentation/secure/viewModels/SecurityViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SecurityViewModel.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/10/14.
+//
+
+import Foundation
---
+++ TellingMe/tellingMe/presentation/secure/views/securityView.swift
@@ -0,0 +1,8 @@
+//
+//  securityView.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 2023/10/14.
+//
+
+import Foundation
