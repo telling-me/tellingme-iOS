4394708471db4759ea79a778752fdaa596589af3
PecanPie <hidra0321@naver.com>
2023-10-17 02:02:45 +0900

    Added Status Model for Security

--- TellingMe/tellingMe/data/secure/model/SecureModel.swift
+++ TellingMe/tellingMe/data/secure/model/SecureModel.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+enum LockStatus {
+    case intact
+    case wrong
+    case correct
+    
+    var intValue: Int {
+        switch self {
+        case .intact:
+            return 0
+        case .wrong:
+            return 1
+        case .correct:
+            return 2
+        }
+    }
+}
+
+enum LockPermissionStatus {
+    case unlocked
+    case withPassword
+    case withBiometry
+}
+
+enum SecurityModeAuthorizedStatus {
+    case unable
+    case disabled
+    case enabled
+}
+
+enum PasswordSettingStatus {
+    case none
+    case passwordNotSet
+    case passwordSet
+}
+
+enum BiometricSettingStatus {
+    case none
+    case biometryNotAvailable
+    case passwordNotSet
+    case passwordSetWithNoBiometry
+    case passwordSetWithBiometry
+}
+
+/// 비밀번호가 입력이 되었는지 확인 하는 모델
+/// - isAdded: 1 일 때는 비밀번호가 입력이 된 상황
+/// - isAdded: 0 일 때는 비밀번호가 비어있는 상황
+struct PasswordCircleModel {
+var isAdded: Int
+}
