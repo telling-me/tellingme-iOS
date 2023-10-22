7fd90f53325981539ee966eb68bfa52331f4a9d5
PecanPie <hidra0321@naver.com>
2023-10-23 02:02:04 +0900

    Now We Use can swipe back to pop

--- TellingMe/tellingMe/util/extension/UIViewController+.swift
+++ TellingMe/tellingMe/util/extension/UIViewController+.swift
@@ -64,3 +64,14 @@ extension UIViewController {
         })
     }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
