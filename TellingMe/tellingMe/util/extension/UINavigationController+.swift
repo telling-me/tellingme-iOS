35069b0c95d70ab25e9d5931745a4ede12d9922b
PecanPie <hidra0321@naver.com>
2023-10-23 02:03:56 +0900

    UINavigationController Extension file is added and the codes are in the right place

--- TellingMe/tellingMe/util/extension/UINavigationController+.swift
+++ TellingMe/tellingMe/util/extension/UINavigationController+.swift
@@ -5,4 +5,15 @@
 //  Created by KYUBO A. SHIM on 2023/10/23.
 //
 
-import Foundation
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
--- TellingMe/tellingMe/util/extension/UIViewController+.swift
+++ TellingMe/tellingMe/util/extension/UIViewController+.swift
@@ -64,14 +64,3 @@ extension UIViewController {
         })
     }
 }
-
-extension UINavigationController: UIGestureRecognizerDelegate {
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
-    }
-    
-    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return viewControllers.count > 1
-    }
-}
