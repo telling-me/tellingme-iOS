604224d11bb4455277668a4df4354a5fee4d3117
PecanPie <hidra0321@naver.com>
2023-11-01 02:26:36 +0900

    Added Secure DTO for password

--- TellingMe/tellingMe/data/secure/dto/SecureDTO.swift
+++ TellingMe/tellingMe/data/secure/dto/SecureDTO.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+/**
+ 비밀번호를 잃어버렸을 때, 팝업창에서 이메일을 보여주기 위한 Response 모델입니다.
+ */
+struct SecureFetchEmailResponse: Codable {
+    let email: String
+}
+
+/**
+ 비밀번호를 잃어버렸을 때, 새로운 비밀번호를 받기 위한 검증용 이메일 Request 모델입니다.
+ */
+struct SecurePostEmailRequest: Codable {
+    let email: String
+}
+
+/**
+ 비밀번호를 잃어버렸을 때, 비밀번호를 초기화하고 이메일을 보내는 Request 모델입니다.
+ */
+struct SecurePostNewSecureValueRequest: Codable {
+    let password: Int
+}
