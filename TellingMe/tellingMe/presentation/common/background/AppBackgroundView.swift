b3bbfa42ab1ca05792d2b76c03bb8aa9da3a1cf3
PecanPie <hidra0321@naver.com>
2023-11-11 23:33:05 +0900

    Added backgroundview for showing logo when background

---
+++ TellingMe/tellingMe/presentation/common/background/AppBackgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  AppBackgroundView.swift
+//  tellingMe
+//
+//  Created by KYUBO A. SHIM on 11/11/23.
+//
+
+import UIKit
+
+class AppBackgroundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
