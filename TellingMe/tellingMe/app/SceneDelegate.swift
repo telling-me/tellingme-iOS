dfe8ed08cf23f520af961b0e8ee4beee38af0d06
PecanPie <hidra0321@naver.com>
2023-11-11 23:56:11 +0900

    Now When the app goes on background the logo view appears

--- TellingMe/tellingMe/app/SceneDelegate.swift
+++ TellingMe/tellingMe/app/SceneDelegate.swift
@@ -13,6 +13,7 @@ import KakaoSDKUser
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private var isOpenedFlagForBackgroundTask = false
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url {
@@ -44,6 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        showSecureImageForBackgroundStatus(isBackground: false)
         print("Re-Called Check")
         NotificationCenter.default.post(name: Notification.Name("RefreshHomeView"), object: nil)
         NotificationCenter.default.post(name: Notification.Name("RefreshAnimation"), object: nil)
@@ -52,16 +54,79 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        showSecureImageForBackgroundStatus(isBackground: true)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        if isOpenedFlagForBackgroundTask == false {
+            isOpenedFlagForBackgroundTask.toggle()
+            return
+        }
+        
+        /// 구독하지 않은 유저는 0 이고, 구독한 유저는 10부터 존재한다.
+        if UserDefaults.standard.integer(forKey: StringLiterals.paidProductId) == 0 {
+            return
+        }
+        
+        if SecurityManager.checkSecurityPermission() == .unlocked {
+            return
+        } else {
+            let elapsedBackgroundTime = UserDefaults.standard.double(forKey: StringLiterals.backgroundStayingTime)
+            let currentTime = Date().timeIntervalSince1970
+            let difference = currentTime - elapsedBackgroundTime
+            
+            /// 백그라운드에서 30분 경과시, 호출된다.
+            if difference >= 1800 {
+                presentSecureViewController()
+            }
+        }
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        showSecureImageForBackgroundStatus(isBackground: true)
+        
+        /// 구독하지 않은 유저는 0 이고, 구독한 유저는 10부터 존재한다.
+        if UserDefaults.standard.integer(forKey: StringLiterals.paidProductId) == 0 {
+            return
+        }
+        
+        if SecurityManager.checkSecurityPermission() == .unlocked {
+            return
+        } else {
+            let currentTime = Date().timeIntervalSince1970
+            UserDefaults.standard.set(currentTime, forKey: StringLiterals.backgroundStayingTime)
+        }
     }
 }
+
+//extension SceneDelegate {
+//    
+//    private func presentSecureViewController() {
+//        let secureViewController = SecurityViewController()
+//        secureViewController.modalPresentationStyle = .overFullScreen
+//        window?.rootViewController?.present(secureViewController, animated: false)
+//    }
+//    
+//    private func showSecureImageForBackgroundStatus(isBackground: Bool) {
+//        let imageTagNumber: Int = -20
+//        let backgroundView = window?.viewWithTag(imageTagNumber)
+//        
+//        if isBackground != false {
+//            if backgroundView == nil {
+//                let backgroundImageView = AppBackgroundView()
+//                backgroundImageView.frame = window!.frame
+//                backgroundImageView.tag = imageTagNumber
+//                window?.addSubview(backgroundImageView)
+//            }
+//        } else {
+//            if let backgroundView {
+//                backgroundView.removeFromSuperview()
+//            }
+//        }
+//    }
+//}
