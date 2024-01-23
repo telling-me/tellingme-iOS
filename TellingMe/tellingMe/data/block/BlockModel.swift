c589a820dab9fc259b234c34ef6634b0de635543
PecanPie <hidra0321@naver.com>
2024-01-23 20:35:03 +0900

    Realm 을 사용한 Block 모델을 추가헀습니다.

--- TellingMe/tellingMe/data/block/BlockModel.swift
+++ TellingMe/tellingMe/data/block/BlockModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+final class BlockModel: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var storyId: Int
+    @Persisted var userId: String
+    
+    convenience init(storyId: Int, userId: String) {
+        self.init()
+        self.storyId = storyId
+        self.userId = userId
+    }
+}
