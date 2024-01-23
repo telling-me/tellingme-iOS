3f497311d27fd7e5638026f1e47d4e5d02ea706b
PecanPie <hidra0321@naver.com>
2024-01-23 20:34:18 +0900

    RealmSwift 를 추가하여 로컬 DB 를 사용합니다.

--- TellingMe/tellingMe/util/managers/RealmManager.swift
+++ TellingMe/tellingMe/util/managers/RealmManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+struct RealmManager {
+    
+    func blockStoryId(of storyId: Int, userIdOf userId: String) {
+        let realm = try! Realm()
+        let newBlockObject = BlockModel(storyId: storyId, userId: userId)
+        try! realm.write({
+            realm.add(newBlockObject)
+        })
+    }
+    
+    func fetchBlockedStories() -> [Int] {
+        let realm = try! Realm()
+        var blockedIds: [Int]
+        blockedIds = Array(realm.objects(BlockModel.self))
+            .map { $0.storyId }
+        return blockedIds
+    }
+}
+
